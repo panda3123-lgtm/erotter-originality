c4638da790e23d9a87d298854787d2f301f7ca14
panda3123-lgtm <panda3.123@icloud.com>
2026-07-13 20:53:40 +0900

    Update card.js

--- card.js
+++ card.js
@@ -224,3 +224,125 @@ function resetSearch(){
 
 
 }
+
+// URLからカード名取得
+
+const params = new URLSearchParams(location.search);
+
+const cardName = params.get("name");
+
+
+
+// cards.json読み込み
+
+fetch("cards.json")
+
+.then(response => response.json())
+
+.then(cards => {
+
+
+
+    const card = cards.find(
+        c => c.name === cardName
+    );
+
+
+
+    const area =
+    document.getElementById("card-detail");
+
+
+
+    if(!card){
+
+        area.innerHTML = `
+
+        <h2>
+        カードが見つかりません
+        </h2>
+
+        `;
+
+        return;
+
+    }
+
+
+
+    area.innerHTML = `
+
+
+    <div class="detail-card">
+
+
+        <img 
+        src="images/cards/${card.image}"
+        alt="${card.name}">
+
+
+        <div class="detail-info">
+
+
+            <h1>
+            ${card.name}
+            </h1>
+
+
+            <p>
+            種類：${card.type}
+            </p>
+
+
+            <p>
+            コスト：${card.cost}
+            </p>
+
+
+
+            ${
+            card.atk ?
+            `<p>ATK：${card.atk}</p>`
+            :
+            ""
+            }
+
+
+
+            <p>
+            色：${card.color}
+            </p>
+
+
+
+            <h2>
+            効果
+            </h2>
+
+
+            <p>
+            ${card.effect}
+            </p>
+
+
+
+            ${
+            card.limit ?
+            `<p class="limit">
+            ⚠ ${card.limit}
+            </p>`
+            :
+            ""
+            }
+
+
+        </div>
+
+
+    </div>
+
+
+    `;
+
+
+});
